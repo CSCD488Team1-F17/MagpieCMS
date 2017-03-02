529dd09499c45638f8f03d564878040e21e17b06
Dsage777 <dsage777@gmail.com>
2017-03-01 22:38:34 -0800

    Reorganized.

--- src/app/frontend/app.js
+++ src/app/frontend/app.js
@@ -2,11 +2,29 @@ $( document ).ready(function(){
     loadApp();
 });
 
+var rootURL = "/api/"
+
 /* Entry point of our application */
 function loadApp(){
     var app = '' +
     '<h1>Hello World!</h1>' +
+    '<div id="test"></div>' +
     '';
 
     $("#app").html(app);
+    test();
+}
+
+function test(){
+    $.ajax({
+			type: 'GET',
+			url: rootURL + 'hello/sage',
+			success: function(response){
+				console.log('Success: ', response);
+				$("#test").html(response);
+			},
+			error: function(xhr, type){
+			   console.log(xhr, type);
+			}
+		});
 }

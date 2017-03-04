882335fe2a4cc7a4397a980a699d0af8d6e8c822
Vlad Sinitsa <vladsinitsa@eagles.ewu.edu>
2017-03-03 21:02:24 -0800

    abstract api endpoints

--- app/src/routes.php
+++ app/src/routes.php
@@ -24,5 +24,54 @@
 	    } else {
    		    echo "0 results";
 	    }
+        $conn->close();
+    });
+
+    $app->get('/api/walk/{wid}', function (Request $request, Response $response){
+        $conn = connect_db();
+        $stmt = $conn->prepare("SELECT * FROM Walks WHERE WID = ?");
+        $stmt->bind_param("i", $request->getAttribute('wid'));
+        $output = $stmt->execute();
+        if ($output->num_rows > 0) {
+    	    while($row = $output->fetch_assoc()) {
+        	    echo json_encode($row);
+    	    }
+	    } else {
+   		    echo "0 results";
+	    }
+        $stmt->close();
+        $conn->close();
+    });
+
+    $app->get('/api/landmark/{lid}', function (Request $request, Response $response){
+        $conn = connect_db();
+        $stmt = $conn->prepare("SELECT * FROM LandMarks WHERE LID = ?");
+        $stmt->bind_param("i", $request->getAttribute('lid'));
+        $output = $stmt->execute();
+        if ($output->num_rows > 0) {
+    	    while($row = $output->fetch_assoc()) {
+        	    echo json_encode($row);
+    	    }
+	    } else {
+   		    echo "0 results";
+	    }
+        $stmt->close();
+        $conn->close();
+    });
+
+    $app->get('/api/landmark/all/{wid}', function (Request $request, Response $response){
+        $conn = connect_db();
+        $stmt = $conn->prepare("SELECT * FROM LandMarks INNER JOIN WalkLandMarks ON WalkLandMarks.LandMarkID = LandMarks.LID WHERE WalkLandMarks.WalkID = ?");
+        $stmt->bind_param("i", $request->getAttribute('wid'));
+        $output = $stmt->execute();
+        if ($output->num_rows > 0) {
+    	    while($row = $output->fetch_assoc()) {
+        	    echo json_encode($row);
+    	    }
+	    } else {
+   		    echo "0 results";
+	    }
+        $stmt->close();
+        $conn->close();
     });
 ?>

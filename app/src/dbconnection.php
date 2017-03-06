2a3ecf18109494c17e40e619b9be77ad378c7ee6
Vlad Sinitsa <vladsinitsa@eagles.ewu.edu>
2017-03-06 14:10:32 -0800

    Update dbconnection.php

--- app/src/dbconnection.php
+++ app/src/dbconnection.php
@@ -2,9 +2,9 @@
 
 function connect_db(){
 	$server = "localhost";
-	$user = "root";
-	$pass = "supersecretpw2016";
-	$database = "magpiecms";
+	$user = "magpiehu_admin";
+	$pass = "";
+	$database = "magpiehu_cmsdb";
 	$connection = new mysqli($server, $user, $pass, $database);
 
 	return $connection;

529641440f322a710915edefe13c8a6243474d9d
Vlad Sinitsa <vladsinitsa@eagles.ewu.edu>
2017-03-17 18:35:38 -0700

    Api call clean up and image test endpoint

--- app/src/routes.php
+++ app/src/routes.php
@@ -25,12 +25,12 @@
         echo json_encode($ara);
         $conn = null;
 
-    $app->get('/api/walk/{wid}', function (Request $request, Response $response){
+    $app->get('/api/walk/{cid}', function (Request $request, Response $response){
         $conn = connect_db();
-        $wid = (int)$request->getAttribute('wid');
+        $cid = (int)$request->getAttribute('cid');
         $stmt = $conn->prepare("SELECT * FROM Walks WHERE WID = ?;");
-        $stmt->execute([$wid]);
-        while($row = $output->fetch()) {
+        $stmt->execute([$cid]);
+        while($row = $stmt->fetch()) {
             echo json_encode($row);
         }
         $conn = null;
@@ -41,22 +41,28 @@
         $lid = (int)$request->getAttribute('lid');
         $stmt = $conn->prepare("SELECT * FROM LandMarks WHERE LID = ?;");
         $stmt->execute([$lid]);
-        while($row = $output->fetch()) {
+        while($row = $stmt->fetch()) {
             echo json_encode($row);
         }
         $conn = null;
     });
 
-    $app->get('/api/landmark/all/{wid}', function (Request $request, Response $response){
+    $app->get('/api/landmark/all/{cid}', function (Request $request, Response $response){
         $ara = array();
         $conn = connect_db();
-        $wid = (int)$request->getAttribute('wid');
+        $cid = (int)$request->getAttribute('cid');
         $stmt = $conn->prepare("SELECT * FROM LandMarks INNER JOIN WalkLandMarks ON WalkLandMarks.LandMarkID = LandMarks.LID WHERE WalkLandMarks.WalkID = ?;");
-        $stmt->execute([$wid]);
-        while($row = $output->fetch()) {
+        $stmt->execute([$cid]);
+        while($row = $stmt->fetch()) {
             array_push($ara, $row);
         }
         echo json_encode($ara);
         $conn = null;
+    });
+
+    $app->get('/image/test', function (Request $request, Response $response){
+        $image = file_get_contents('../Resources/Images/test.jpg');
+        $response->write($image);
+        return $response->withHeader('Content-Type', 'image/jpg');
     });
 ?>

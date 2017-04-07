06ada48c24b0aa157129b2225ad76f82b86745a6
Vlad Sinitsa <vladsinitsa@eagles.ewu.edu>
2017-04-07 16:22:40 -0700

    fix again

--- app/public_html/index.php
+++ app/public_html/index.php
@@ -8,11 +8,11 @@
     $container = $app->getContainer();
 
     $container['view'] = function ($container) {
-        $view = new \Slim\Views\Twig('templates', [
+        $view = new \Slim\Views\Twig('../templates', [
             'cache' => 'false'
         ]);
         
-        $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '../templates');
+        $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
         $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
 
         return $view;

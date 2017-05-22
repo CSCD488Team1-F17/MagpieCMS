75c21fa27912eaaf36e9a467da71992209b24438
Nick Duke <duken8@eagles.ewu.edu>
2017-05-21 19:58:45 -0700

    Awards page fully implemented
    Fully functional google maps and fills all fields of database for awards

--- app/src/routes/api.php
+++ app/src/routes/api.php
@@ -573,12 +573,12 @@
 	$app->post('/add/awards', function(Request $request){
 		$cid =(int)$request->getParam("cid");
 		$name = $request->getParam("name");
-		//$lat = $request->getParam("latitude");
-		//$long = $request->getParam("longitude");
+		$lat = $request->getParam("latitude");
+		$long = $request->getParam("longitude");
 		$optionalConditions = $request->getParam("optionalConditions");
 		$conn = connect_db();	
-		$stmt = $conn->prepare("INSERT INTO Awards (CID, Name, optionalConditions) VALUES (?, ?, ?);");//add long/lat
-		$stmt->execute([$cid, $name, $optionalConditions]);//add long/lat
+		$stmt = $conn->prepare("INSERT INTO Awards (CID, Name, Latitude, Longitude, optionalConditions) VALUES (?, ?, ?, ?, ?);");
+		$stmt->execute([$cid, $name, $lat, $long, $optionalConditions]);
 		$conn = null;
 	});
 ?>
